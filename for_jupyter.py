2c7608c679984fce64b3dbf584f7eb7a1736dafb
Ivan Abraham <ivan.abrms@gmail.com>
2018-06-29 11:44:38 -0500

    Updated Python notebook with examples

--- for_jupyter.py
+++ for_jupyter.py
@@ -4,15 +4,10 @@ import gzip, pickle
 # all_data = tsc.TsCluster()
 # all_data.from_directory()
 
-file_name = './data_binary_roi33.bin'
+file_name = './binaries/data_binary_roi33.bin'
 print('Starting gzip')
 with gzip.open(file_name, 'rb') as file:
     print('Starting pickle')
     all_data = pickle.load(file)
     print('End pickle')
-
-hl_samples = [sample['TimeSeries'] for sample in all_data.samples if sample['Group']=='hl' and sample['Run']!='run3']
-
-with open('./factorized_by_groups.bin', 'rb') as file:
-    factor_list = pickle.load(file)
 
